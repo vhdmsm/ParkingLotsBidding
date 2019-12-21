0bf9b9baeb5e67e441bc4550fcef2ffb27bb13ea
Vahid <vhdmsm@gmail.com>
2019-12-21 12:53:03 +0330

    settings

--- parking/parking/prod_settings.py
+++ parking/parking/prod_settings.py
@@ -77,16 +77,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'parking.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'parking',
+        'HOST': 'db',
+        'PASSWORD': 'p@arking',
+	    'CONN_MAX_AGE': 60,
+    }
+}
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
